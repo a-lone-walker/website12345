093aafa21b461a77c2b07f33ecb5e9dd81151e3b
12345-Charanteja <162510492+12345-Charanteja@users.noreply.github.com>
2024-05-25 11:05:17 +0530

    Add files via upload

---
+++ js-basics/portfolio.js
@@ -0,0 +1,11 @@
+// Add JavaScript functionality here if needed
+// For example, you can add smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
